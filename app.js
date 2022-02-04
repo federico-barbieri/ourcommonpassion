242f69983aa993960dd8999aac0acc101135c60e
Federico Barbieri <federicobarbieri54@gmail.com>
2022-02-04 04:40:46 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ welcome.addEventListener('mouseover', () => {
 welcome.addEventListener('mouseout', () => {
   welcome.innerHTML = "WELCOME TO OUR COMMON PASSION";
 })
+
+const game = document.querySelector('pizza-game');
+
+const pizza = document.querySelector('.pizza-gamy');
+
+pizza.addEventListener('click', () => {
+  pizza.style.display = "none";
+
+})
